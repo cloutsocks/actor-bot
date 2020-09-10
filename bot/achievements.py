860695eabe76f69f1954113e537036cc362ace62
Jacob B <jacob@rgby.xyz>
2020-09-09 20:51:16 -0500

    Add achievements

--- bot/achievements.py
+++ bot/achievements.py
@@ -30,8 +30,11 @@ class Achievements(commands.Cog):
             msg = f'{msg}\n{how}'
 
         msg = f'{msg}{FIELD_BREAK}'
+        
+        sent = await channel.send(msg)
+        for reaction in ['<:GVHtrishyay:721410319494152243>', '<:lovehonk:722884312378376192>']:
+            await sent.add_reaction(reaction.strip('<>'))
 
-        await channel.send(msg)
         await self.bot.get_channel(753419964035235890).send(msg)
 
 def setup(bot):
